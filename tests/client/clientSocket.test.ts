b523c661faf7f1b2419f2a2736fe9a0256ce8cf8
Okpala76 <okpalaogochukwu76@gmail.com>
2026-04-10 17:07:13 +0100

    test: test ... can receive a message from the server

--- tests/client/clientSocket.test.ts
+++ tests/client/clientSocket.test.ts
@@ -1,9 +1,43 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { ClientSocket } from '../../client/clientSocket.ts';
+import { ServerMessage } from '../../shared/protocol.ts';
 
 describe('ClientSocket (null)', () => {
   it('is not connected before connect is called', () => {
     const socket = ClientSocket.createNull();
     expect(socket.isConnected).toBe(false);
   });
+  it('is connected after connect is called', async () => {
+    const socket = ClientSocket.createNull();
+
+    await socket.connect();
+
+    expect(socket.isConnected).toBe(true);
+  });
+  it('is not connected after close is called', async () => {
+    const socket = ClientSocket.createNull();
+
+    await socket.connect();
+    await socket.close();
+
+    expect(socket.isConnected).toBe(false);
+  });
+  it('can recieve a message from the server', async () => {
+    const message: ServerMessage = { type: 'ready', id: '1' };
+    const socket = ClientSocket.createNull();
+    const tracker = socket.trackMessages();
+    await socket.connect();
+    const server = socket.simulateServer();
+    server.send(message);
+    expect(tracker.data).toHaveLength(1);
+    expect(tracker.data[0]).toEqual({ type: 'ready', id: '1' });
+  });
+  //   it('can send a message to the server', async () => {
+  //     const message: UnsubscribeMsg = { type: 'unsubscribe', id: '1' };
+
+  //     const socket = ClientSocket.createNull();
+  //     const tracker = socket.trackMessages();
+  //     await socket.connect();
+  //     const server = socket.simulateServer();
+  //   });
 });
