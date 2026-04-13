79e3c6fb1aabbfe61912bbca69ab9cfa02e9af93
Iam_Ebuka <ebukagrizz@gmail.com>
2026-04-13 15:46:33 +0100

    test: green - connects to a real server

--- tests/client/clientSocket.integration.test.ts
+++ tests/client/clientSocket.integration.test.ts
@@ -1,21 +1,28 @@
-import { afterEach } from 'node:test';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, afterEach, describe, expect, it } from 'vitest';
 import { ClientSocket } from '../../client/clientSocket.ts';
 import { WebSocketWrapper } from '../../server/infrastructure/websocket.ts';
+import { createServer } from '../../server/index.ts';
 
 describe('ClientSocket (real)', () => {
   const PORT = 9877;
-  let server: WebSocketWrapper;
+  let webSocketServer: WebSocketWrapper;
+  let server: Awaited<ReturnType<typeof createServer>>;
+
+  beforeEach(async () => {
+    server = await createServer();
+  });
 
   afterEach(async () => {
+    // await webSocketServer.close();
     await server.close();
   });
 
   it('connects to a real server', async () => {
-    server = WebSocketWrapper.createRawWs({ port: PORT });
+    webSocketServer = WebSocketWrapper.createRawWs({ port: PORT });
+    await webSocketServer.start();
     const client = ClientSocket.create('ws://localhost:9877');
     await client.connect();
     expect(client.isConnected).toBe(true);
-    expect(server.clientCount).toBe(1);
+    expect(webSocketServer.clientCount).toBe(1);
   });
 });
