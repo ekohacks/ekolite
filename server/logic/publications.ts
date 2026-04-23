81c80e00ae2adb77457e744b3cd9dd4c5112c5a1
Ebuka Odiah <ebukagrizz@gmail.com>
2026-04-23 15:05:53 +0100

    Eko 231/3a2 send initial documents on subscribe (#29)
    * test: red - sends initial documents and ready signal on subscribe
    
    * test: green - sends initial documents and ready signal on subscribe
    
    * refactor: publications - sends initial documents and ready signal on subscribe
    
    * test: green - sends ready even when no documents exist
    
    * refactor: publications and sends initial documemnts and ready signal on subscribe
    
    * refactor: changed toAddedMsg and toReadyMsg to addedMessage and readyMessage respectively

--- server/logic/publications.ts
+++ server/logic/publications.ts
@@ -4,6 +4,18 @@ import { WebSocketWrapper } from '../infrastructure/websocket.ts';
 
 type PublicationDef = () => { collection: string; query: object };
 
+const addedMessage = (collection: string, doc: Record<string, unknown>) => ({
+  type: 'added',
+  collection,
+  id: doc._id as string,
+  fields: Object.fromEntries(Object.entries(doc).filter(([key]) => key !== '_id')),
+});
+
+const readyMessage = (subId: string) => ({
+  type: 'ready',
+  id: subId,
+});
+
 export class Publications {
   private publications = new Map<string, PublicationDef>();
   private ws: WebSocketWrapper;
@@ -21,6 +33,7 @@ export class Publications {
   async handleMessage(clientId: string, message: ClientMessage): Promise<void> {
     if (message.type === 'subscribe') {
       const queryFn = this.publications.get(message.name);
+
       if (!queryFn) {
         this.ws.send(clientId, {
           type: 'error',
@@ -29,6 +42,14 @@ export class Publications {
         });
         return Promise.resolve();
       }
+
+      const { collection, query } = queryFn();
+      const docs = await this.mongo.find<{ _id: string }>(collection, query);
+
+      for (const doc of docs) {
+        this.ws.send(clientId, addedMessage(collection, doc));
+      }
+      this.ws.send(clientId, readyMessage(message.id));
     }
     return Promise.resolve();
   }
--- tests/logic/publications.test.ts
+++ tests/logic/publications.test.ts
@@ -22,4 +22,55 @@ describe('Publications', () => {
       error: { code: 404, message: 'Unknown publication: nonexistent' },
     });
   });
+
+  it('sends initial documents and ready signal on subscribe', async () => {
+    const mongo = MongoWrapper.createNull({
+      find: [[{ _id: '1', name: 'existing.bam' }]],
+    });
+    const ws = WebSocketWrapper.createNull();
+    const client = ws.simulateConnection();
+    const pubs = new Publications(mongo, ws);
+
+    pubs.define('files.all', () => ({ collection: 'files', query: {} }));
+
+    await pubs.handleMessage(client.id, {
+      type: 'subscribe',
+      id: 'sub1',
+      name: 'files.all',
+    });
+
+    expect(client.messages).toHaveLength(2);
+    expect(client.messages).toEqual([
+      {
+        type: 'added',
+        collection: 'files',
+        id: '1',
+        fields: { name: 'existing.bam' },
+      },
+      {
+        type: 'ready',
+        id: 'sub1',
+      },
+    ]);
+  });
+
+  it('sends ready even when no documents exist', async () => {
+    const mongo = MongoWrapper.createNull({
+      find: [[]],
+    });
+    const ws = WebSocketWrapper.createNull();
+    const client = ws.simulateConnection();
+    const pubs = new Publications(mongo, ws);
+
+    pubs.define('files.all', () => ({ collection: 'files', query: {} }));
+
+    await pubs.handleMessage(client.id, {
+      type: 'subscribe',
+      id: 'sub1',
+      name: 'files.all',
+    });
+
+    expect(client.messages).toHaveLength(1);
+    expect(client.messages[0]).toEqual({ type: 'ready', id: 'sub1' });
+  });
 });
