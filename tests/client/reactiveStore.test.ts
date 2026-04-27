5c1c7290df53c5426ade36be882489b5de6f137d
Iam_Ebuka <ebukagrizz@gmail.com>
2026-04-27 17:22:35 +0100

    test: red - calls registered listeners when a document is added

--- tests/client/reactiveStore.test.ts
+++ tests/client/reactiveStore.test.ts
@@ -16,17 +16,33 @@ describe('ReactiveStore', () => {
   });
 
   it('returns a single document by id', () => {
-  const store = new ReactiveStore();
+    const store = new ReactiveStore();
 
-  store.handleMessage({
-    type: 'added',
-    collection: 'files',
-    id: '1',
-    fields: { name: 'existing.bam' },
+    store.handleMessage({
+        type: 'added',
+        collection: 'files',
+        id: '1',
+        fields: { name: 'existing.bam' },
+    });
+
+    expect(store.getById('1')).toEqual({ _id: '1', name: 'existing.bam' });
+    expect(store.getById('does-not-exist')).toBeUndefined();
   });
 
-  expect(store.getById('1')).toEqual({ _id: '1', name: 'existing.bam' });
-  expect(store.getById('does-not-exist')).toBeUndefined();
-});
+  it('calls registered listeners when a document is added', () => {
+    const store = new ReactiveStore();
+    let callCount = 0;
+    store.onChange(() => {
+        callCount++;
+    });
 
+    store.handleMessage({
+        type: 'added',
+        collection: 'files',
+        id: '1',
+        fields: { name: 'existing.bam' },
+    });
+
+    expect(callCount).toBe(1);
+  });
 });
