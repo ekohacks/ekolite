6589cddddf60a886f8a7e7dd4b6020bb820ced9b
Iam_Ebuka <ebukagrizz@gmail.com>
2026-04-27 15:47:28 +0100

    test: red - inserts a document on added message

---
+++ tests/client/reactiveStore.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import { ReactiveStore } from '../../client/reactiveStore.ts';
+
+describe('ReactiveStore', () => {
+  it('inserts a document on added message', () => {
+    const store = new ReactiveStore();
+
+    store.handleMessage({
+      type: 'added',
+      collection: 'files',
+      id: '1',
+      fields: { name: 'existing.bam' },
+    });
+
+    expect(store.getAll()).toEqual([{ _id: '1', name: 'existing.bam' }]);
+  });
+});
