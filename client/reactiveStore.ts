b2420604504caf84549226fcd16da91c15ae0f9d
Iam_Ebuka <ebukagrizz@gmail.com>
2026-04-27 16:07:52 +0100

    test: green - inserts a document on added message

---
+++ client/reactiveStore.ts
@@ -0,0 +1,18 @@
+import { DataMsg } from '../shared/protocol.ts';
+
+export class ReactiveStore {
+  private serverMessages = new Map<string, Record<string, unknown>>();
+
+  handleMessage(msg: DataMsg): void {
+    if (msg.type === 'added') {
+      this.serverMessages.set(msg.id, msg?.fields ?? {});
+    }
+  }
+
+  getAll(): Record<string, unknown>[] {
+    return Array.from(this.serverMessages.entries()).map(([id, fields]) => ({
+      _id: id,
+      ...fields,
+    }));
+  }
+}
