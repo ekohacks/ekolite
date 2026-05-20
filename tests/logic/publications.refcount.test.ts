dd32349960bec75f934b526eb0e42c23bb6199c7
Iam_Ebuka <ebukagrizz@gmail.com>
2026-05-20 15:45:46 +0100

    test: red - unsubscribing one sub does not remove a doc the other still covers

---
+++ tests/logic/publications.refcount.test.ts
@@ -0,0 +1,28 @@
+import { expect, it } from 'vitest';
+import { Publications } from '../../server/logic/publications.ts';
+import { MongoWrapper } from '../../server/infrastructure/mongo.ts';
+import { WebSocketWrapper } from '../../server/infrastructure/websocket.ts';
+
+it('unsubscribing one sub does not remove a doc the other still covers', async () => {
+  const mongo = MongoWrapper.createNull({
+    find: [[{ _id: '1', name: 'shared.bam' }], [{ _id: '1', name: 'shared.bam' }]],
+  });
+  const ws = WebSocketWrapper.createNull();
+  const client = ws.simulateConnection();
+  const pubs = new Publications(mongo, ws);
+
+  pubs.define('files.all', () => ({ collection: 'files', query: {} }));
+  pubs.define('files.featured', () => ({ collection: 'files', query: {} }));
+
+  await pubs.handleMessage(client.id, { type: 'subscribe', id: 'subA', name: 'files.all' });
+  await pubs.handleMessage(client.id, { type: 'subscribe', id: 'subB', name: 'files.featured' });
+
+  const countBeforeUnsub = client.messages.length;
+  await pubs.handleMessage(client.id, { type: 'unsubscribe', id: 'subA' });
+
+  const removedForSharedDoc = client.messages
+    .slice(countBeforeUnsub)
+    .find((m) => (m as { type: string }).type === 'removed');
+
+  expect(removedForSharedDoc).toBeUndefined();
+});
